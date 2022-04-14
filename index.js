d1a414f0c71deb411c24edd7221928d64b46f2b0
Srinivas <seenuchaparala@gmail.com>
2022-04-14 14:21:59 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let txt = document.getElementById('text');
+    txt.innerHTML = 'This is really cool!'
+  });
